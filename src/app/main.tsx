bb71088ea8b5c10bd178ef5796fb968f0bd54821
Sasha <73294558+2JustFox2@users.noreply.github.com>
2026-02-25 20:22:25 +0300

    Refactor styles and remove unused Test component

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -4,7 +4,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 
 import { HomePage, Login, RegisterPhoneNumber, RegisterSmsCode, RegisterRole, ChoiseCards } from '@pages';
-import Test from '@pages/test';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
@@ -19,8 +18,6 @@ createRoot(document.getElementById('root')!).render(
                 />
                 <Route path="/register/role" element={<RegisterRole />} />
                 <Route path="/cards" element={<ChoiseCards/>} />
-                <Route path="/test" element={<Test/>} />
-                
             </Routes>
         </BrowserRouter>
     </StrictMode>,
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,5 +3,4 @@ export { default as Login } from './login';
 export { default as RegisterPhoneNumber } from './register/phone-number';
 export { default as RegisterSmsCode } from './register/sms-code';
 export { default as RegisterRole } from './register/role';
-export { default as Test } from './test/';
 export { ChoiseCards } from './catalog';
